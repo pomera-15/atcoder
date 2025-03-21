aa5c974c66792e1911d7122e8f07e1af0bfee916
pomera-15 <shoheiyamanashi1204@gmail.com>
2025-03-21 22:09:22 +0900

    微修正

--- BeginnersSelection/8_ABC085C.py
+++ BeginnersSelection/8_ABC085C.py
@@ -1,11 +1,11 @@
-N, Y = input().split()
-N, Y = int(N), int(Y)
+# N枚のお札の合計金額がY円
+N, Y = map(int, input().split())
 
-ans_x = -1
-ans_y = -1
-ans_z = -1
-for x in range(N + 1):
-    for y in range(N + 1 - x):
+ans_x = -1  # 10000
+ans_y = -1  # 5000
+ans_z = -1  # 1000
+for x in range(0, N + 1):
+    for y in range(0, N + 1 - x):
         z = N - x - y
         if 10000*x + 5000*y + 1000*z == Y:
             ans_x = x
