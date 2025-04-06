d29efb0c4a8c3fc84de5eb6b8ebefc41f97e0fd6
pomera-15 <shoheiyamanashi1204@gmail.com>
2025-04-06 22:55:01 +0900

    add 8 ruidai

--- BeginnersSelection/8_ruidai_ABC057C.py
+++ BeginnersSelection/8_ruidai_ABC057C.py
@@ -1,1 +1,13 @@
-zange
+import math
+N = int(input())
+
+min_k = 11
+for a in range(1, int(math.sqrt(N))+1):
+    if N % a == 0:
+        b = N // a
+        k = max([len(str(a)), len(str(b))])
+        if min_k > k:
+            min_k = k
+
+print(min_k)
+
